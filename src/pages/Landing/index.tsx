ae335cddb92fc00406c201d53bf947739e3e833e
Rodrigo Barbosa <rodrigo.lima@develcode.com.br>
2022-04-19 19:54:01 -0300

    chore(SPCRT-0000): fixing services to get data, put, post and more

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
+import api from '../../services/api';
+
 function LandingContainer() {
+  async function handleFetchProducts() {
+    const { data } = await api.get('/products');
+  }
+
+  React.useEffect(() => {
+    handleFetchProducts();
+  }, []);
+
   return (
     <section id="landing-page-container">
       <h4>Shopping card landing</h4>
