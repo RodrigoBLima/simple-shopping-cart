4dc9bfa5804ee332abbcda2f73c59a6389aab340
Rodrigo Barbosa <rodrigo.lima@develcode.com.br>
2022-04-21 19:19:06 -0300

    feat(SPCRT-0000): fixing not found page

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import { Container, Typography } from '@mui/material';
+
 export default function NotFoundRoute() {
-  return <section id="not-found-page-container">NotFoundRoute</section>;
+  return (
+    <Container>
+      <Typography variant="h6" gutterBottom component="div">
+        Ooops! Esta página não foi encontrada
+      </Typography>
+      <Link to="/">Ir para página inicial</Link>
+    </Container>
+  );
 }
