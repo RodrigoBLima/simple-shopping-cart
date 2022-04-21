31868a68685ff8f82ab796f4f32da167cc7fd775
Rodrigo Barbosa <rodrigo.lima@develcode.com.br>
2022-04-21 15:45:58 -0300

    feat(SPCRT-0000): fixing material to ui on project

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
 import { CartProvider } from '../contexts/cart/CartContextProvider';
 import Routes from '../routes';
 
+declare module '@mui/material/styles' {
+  interface Theme {
+    status: {
+      danger: string;
+    };
+  }
+  // allow configuration using `createTheme`
+  interface ThemeOptions {
+    status?: {
+      danger?: string;
+    };
+  }
+}
+
+const theme = createTheme({
+  status: {
+    danger: orange[500]
+  }
+});
+
 export default function Main() {
   return (
-    <CartProvider>
-      <Routes />
-    </CartProvider>
+    <ThemeProvider theme={theme}>
+      <CartProvider>
+        <Routes />
+      </CartProvider>
+    </ThemeProvider>
   );
 }
