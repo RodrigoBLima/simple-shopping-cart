e10254a1282af68f18e1548f3f52a257d5a9abc8
Rodrigo Barbosa <rodrigo.lima@develcode.com.br>
2022-04-22 13:30:51 -0300

    feat(SPCRT-0000): fixing container landing test

--- src/tests/pages/Landing/Landing.test.tsx
+++ src/tests/pages/Landing/Landing.test.tsx
@@ -1,1 +1,19 @@
 // todo check page container title `Produtos disponíveis`
+import React from 'react';
+
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import { CartProvider } from '../../../contexts/cart/CartContextProvider';
+import LandingContainer from '../../../pages/Landing';
+
+test('container landing has title', async () => {
+  const { getByText } = render(
+    <CartProvider>
+      <LandingContainer />
+    </CartProvider>,
+    { wrapper: BrowserRouter }
+  );
+
+  expect(getByText('Produtos disponíveis')).toBeInTheDocument();
+});
