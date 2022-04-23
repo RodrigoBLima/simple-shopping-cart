944e73911ec2877bc0ec66bef7570dd0aafa2af1
Rodrigo Barbosa Lima <rodrigo.lima@develcode.com.br>
2022-04-23 09:21:10 -0300

    feat(SPCRT-0000): fixing mock data

--- src/tests/mocks/cart.ts
+++ src/tests/mocks/cart.ts
@@ -1,3 +1,5 @@
+import { ProductProps } from '../../types/products';
+
 export const cartProducts = [
   {
     id: 15,
@@ -86,21 +88,19 @@ export const cartProducts = [
   }
 ];
 
-export const cartProduct = [
-  {
-    id: 20,
-    title: 'DANVOUY Womens T Shirt Casual Cotton Short',
-    price: 12.99,
-    quantity: 1,
-    description:
-      '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
-    category: "women's clothing",
-    image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
-    rating: {
-      rate: 3.6,
-      count: 145
-    }
+export const cartProduct: ProductProps = {
+  id: 20,
+  title: 'DANVOUY Womens T Shirt Casual Cotton Short',
+  price: 12.99,
+  quantity: 1,
+  description:
+    '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.',
+  category: "women's clothing",
+  image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
+  rating: {
+    rate: 3.6,
+    count: 145
   }
-];
+};
 
 export const emptyCartProducts = [];
